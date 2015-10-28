d6c214c88d055c1c89edb2ae75e152d973580c12
nathants <me@nathants.com>
2015-10-28 13:16:32 -0700

    rm dead code

--- tokumx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
-echo "deb http://repo.percona.com/apt "$(lsb_release -sc)" main" | sudo tee /etc/apt/sources.list.d/percona.list
-echo "deb-src http://repo.percona.com/apt "$(lsb_release -sc)" main" | sudo tee -a /etc/apt/sources.list.d/percona.list
-sudo apt-get update
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y tokumx-enterprise
