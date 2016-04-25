c89efed9a4e8c1360893847ff18200342a681587
nathants <me@nathants.com>
2016-04-24 23:12:54 -0700

    emacs

---
+++ emacs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+mkdir -p ~/opt
+cd ~/opt
+sudo apt-get install -y build-essential wget libncurses5-dev libgnutls-dev
+rm -rf emacs*
+wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.xz
+tar xf emacs*
+cd emacs*
+./configure
+make
+make install
