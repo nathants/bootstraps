45e651866c824062be8f29787a1816b429aa139b
nathants <me@nathants.com>
2016-08-01 21:32:08 -0700

    add version arg to postgres

--- postgresql.sh
+++ postgresql.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 set -e
 
+version=$1
+echo install postgresql ${version}
+
 echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y postgresql-9.5
+sudo apt-get install -y postgresql-${version}
 echo "
 # wide open on all ports
 local all all trust
 local all all trust
 host all all 127.0.0.1/32 trust
 host all all ::1/128 trust
-" | sudo tee /etc/postgresql/9.5/main/pg_hba.conf
+" | sudo tee /etc/postgresql/${version}/main/pg_hba.conf
 sudo -u postgres createuser --superuser $(whoami)
 sudo -u postgres createdb $(whoami)
