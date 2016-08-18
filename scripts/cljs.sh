8ba6a70f95706f2a551f0b6c907176cb4e15ba6b
nathants <me@nathants.com>
2016-08-17 20:30:48 -0700

    cljs

---
+++ scripts/cljs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+bootstraps=$(dirname $0)
+bash ${bootstraps}/lein_java8.sh
+bash ${bootstraps}/node6.sh
+
+sudo apt-get update
+sudo apt-get -y install python-pip git
+sudo pip install edn_format
+
+git clone https://github.com/nathants/cljs
+sudo mv cljs/bin/* /usr/bin
+
+cljs ./cljs/examples/shell.cljs
