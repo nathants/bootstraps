730cc1d218718c7f71c24cecb5e8b9e2d66addfa
nathants <me@nathants.com>
2016-08-21 23:59:16 -0700

    drop python from runclj

--- scripts/runcljs.sh
+++ scripts/runcljs.sh
@@ -5,10 +5,6 @@ bootstraps=$(dirname $0)
 bash ${bootstraps}/lein_java8.sh
 bash ${bootstraps}/node6.sh
 
-sudo apt-get update
-sudo apt-get -y install python-pip git
-sudo pip install edn_format
-
 git clone https://github.com/nathants/runclj
 sudo mv runclj/bin/.lein runclj/bin/* /usr/bin
 
