182ee96fa8381362ba7d048af5ec62e29a468a74
nathants <me@nathants.com>
2016-08-22 23:59:47 -0700

    make set_opt more robust

--- scripts/limits.sh
+++ scripts/limits.sh
@@ -2,8 +2,7 @@
 set -e
 
 bootstraps=$(dirname $0)
-source ${bootstraps}/set_opt.sh
 
-set_opt /etc/sysctl.conf 'fs.file-max' '= 120000'
-set_opt /etc/sysctl.conf 'fs.inotify.max_user_watches' '= 120000'
-set_opt /etc/security/limits.conf '* - nofile' '120000'
+bash $bootstraps/set_opt.sh /etc/sysctl.conf 'fs.file-max' '= 120000'
+bash $bootstraps/set_opt.sh /etc/sysctl.conf 'fs.inotify.max_user_watches' '= 120000'
+bash $bootstraps/set_opt.sh /etc/security/limits.conf '* - nofile' '120000'
--- scripts/set_opt.sh
+++ scripts/set_opt.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 set -e
 
-set_opt() {
-    dst=$1
-    head=$2
-    tail=$3
-    sudo sed -i "/${head}/d" ${dst}
-    sudo sed -i "$ a ${head} ${tail}" ${dst}
-    echo updated \"${head} ${tail}\" in ${dst}
+dst=$1
+head=$2
+tail=$3
+head_escaped=$(echo "$head" | sed -r 's/([:\/\*\+\?])/\\\1/')
+tail_escaped=$(echo "$tail" | sed -r 's/([:\/\*\+\?])/\\\1/')
+uncommented_config() {
+    sed -n "/^ *[^#\/\;]*${head_escaped}/p" ${dst}
 }
+[ -f $dst ] || (sudo touch $dst && echo created: $dst)
+if [ -z "$(uncommented_config)" ]; then
+    echo  appended to config: ${dst}
+    echo "$head $tail" | sudo tee -a ${dst} >/dev/null
+    echo "" new: "$(uncommented_config)"
+else
+    echo update config: ${dst}
+    echo "" old: "$(uncommented_config)"
+    sudo sed -i "s:${head_escaped}.*:${head_escaped} ${tail_escaped}:" ${dst}
+    echo "" new: "$(uncommented_config)"
+fi
