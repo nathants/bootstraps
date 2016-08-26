676fdf11cfd0e5f5dc1400a1698a50054b31604c
nathants <me@nathants.com>
2016-08-25 23:51:29 -0700

    cassandra

--- scripts/cassandra_cluster.sh
+++ scripts/cassandra_cluster.sh
@@ -20,17 +20,12 @@ name="cassandra-${cluster_name}"
 ids=$(ec2 new $name --type ${ec2_type} ${spot_price} --gigs ${ec2_gigs} --ami trusty --num ${num_instances})
 ips=$(ec2 ssh $ids -qyc "ifconfig eth0 |grep 'inet addr'|cut -d: -f2|cut -d' ' -f1")
 
-ops_id=$(echo "$ids"|head -n1)
-ops_addr=$(ec2 ssh $ops_id -qyc "ifconfig eth0 |grep 'inet addr'|cut -d: -f2|cut -d' ' -f1")
-
-ec2 tag $ops_id opscenter=true -y
-
 seeds=$(echo "$ips" | head -n3 | tr '\n' ', '| sed 's:.$::')
 
 ec2 ssh $ids -yc "
 
-curl -L https://github.com/nathants/bootstraps/tarball/0a963cd660a2b519e94ee2751cf7794f3a07d3ef | tar zx
+curl -L https://github.com/nathants/bootstraps/tarball/85bfc979230271bc18d178c9ca3a21817b1dfa45 | tar zx
 mv nathants-bootstraps* bootstraps
-bash bootstraps/scripts/cassandra.sh $version $cluster_name $seeds $ops_addr
+bash bootstraps/scripts/cassandra.sh $version $cluster_name $seeds
 
 "
