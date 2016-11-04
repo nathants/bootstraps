0865b28807ae4cd23e6d817b3f434aa316bf84f0
nathants <me@nathants.com>
2016-11-03 20:32:52 -0700

    prompt helper fn

---
+++ scripts/_prompt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu
+
+prompt() {
+    # usage:
+    # > source ./_prompt.sh
+    # > foo=bar
+    # > num=3
+    # > prompt foo num
+    # >> foo: bar
+    # >> instance_type: m3.xlarge
+    # >>
+    # >> proceed? y/n
+    for x in $@; do echo ${x}: ${!x}; done
+    [ "${yes:-n}" = "y" ] || (echo -e "\nproceed? y/n" && read -n1 answer && echo && [ $answer = y ] && echo -e '\nproceeding...\n' || (echo -e '\naborting...\n' && exit 1))
+}
