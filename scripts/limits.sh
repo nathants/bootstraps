5fed7cc72826d9a96d5a63924f2ddca4fe5c2032
nathants <me@nathants.com>
2016-11-11 22:34:10 -0800

    carpet bomb limits

--- scripts/limits.sh
+++ scripts/limits.sh
@@ -5,4 +5,13 @@ bootstraps=$(dirname $0)
 
 bash $bootstraps/set_opt.sh /etc/sysctl.conf 'fs.file-max' ' = 120000'
 bash $bootstraps/set_opt.sh /etc/sysctl.conf 'fs.inotify.max_user_watches' ' = 120000'
+
 bash $bootstraps/set_opt.sh /etc/security/limits.conf '* - nofile' ' 120000'
+bash $bootstraps/set_opt.sh /etc/security/limits.conf '* - memlock' ' unlimited'
+bash $bootstraps/set_opt.sh /etc/security/limits.conf '* - nproc' ' 32768'
+bash $bootstraps/set_opt.sh /etc/security/limits.conf '* - as' ' unlimited'
+
+bash $bootstraps/set_opt.sh /etc/security/limits.conf 'root - nofile' ' 120000'
+bash $bootstraps/set_opt.sh /etc/security/limits.conf 'root - memlock' ' unlimited'
+bash $bootstraps/set_opt.sh /etc/security/limits.conf 'root - nproc' ' 32768'
+bash $bootstraps/set_opt.sh /etc/security/limits.conf 'root - as' ' unlimited'
