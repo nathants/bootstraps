73efbc373722a5aa110b7b072f2bd4471ea6fb4f
nathants <me@nathants.com>
2016-11-12 20:57:37 -0800

    bugfix apt install

--- scripts/java6.sh
+++ scripts/java6.sh
@@ -2,7 +2,7 @@
 set -eu
 
 sudo apt-get update
-sudo apt-get install -y python-software-properties software-properties-common
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 sudo echo oracle-java6-installer shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
