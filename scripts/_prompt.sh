f4cfabe4dac0fc6be01d645a10baf54d2cc02b40
nathants <me@nathants.com>
2016-11-12 21:09:36 -0800

    prompt echoing goes to stderr

--- scripts/_prompt.sh
+++ scripts/_prompt.sh
@@ -11,6 +11,6 @@ prompt() {
     # >> instance_type: m3.xlarge
     # >>
     # >> proceed? y/n
-    for x in $@; do echo ${x}: ${!x}; done
+    for x in $@; do echo ${x}: ${!x} 1>&2; done
     [ "${yes:-n}" = "y" ] || (echo -e "\nproceed? y/n" && read -n1 answer && echo && [ $answer = y ] && echo -e '\nproceeding...\n' || (echo -e '\naborting...\n' && exit 1))
 }
