d1cbbb0bb779d0092b2b6c4023908d007d51b198
nathants <me@nathants.com>
2017-04-10 23:28:43 -0700

    bugfix lz4

--- scripts/lz4.sh
+++ scripts/lz4.sh
@@ -6,7 +6,7 @@ rm -rf lz4*
 curl -L https://github.com/lz4/lz4/tarball/7bb64ff | tar zx
 cd lz4*
 make
-make install
+mv -f lz4 /usr/local/bin
 
 # xxhash
 cd /tmp
@@ -14,4 +14,4 @@ rm -rf Cyan4973*
 curl -L https://github.com/Cyan4973/xxHash/tarball/88c6ee1 | tar zx
 cd Cyan4973*
 make
-mv xxhsum /usr/local/bin
+mv -f xxhsum /usr/local/bin
