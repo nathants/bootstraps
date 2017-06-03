73de74eea7d3e5d8bd900a340a58865b8c2d34a7
nathants <me@nathants.com>
2017-06-02 21:51:42 -0700

    java should add an env variable to bashrc

--- scripts/java8.sh
+++ scripts/java8.sh
@@ -7,3 +7,8 @@ sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 sudo apt-get -y install oracle-java8-installer
+
+mv ~/.bashrc ~/.bashrc.bak
+echo 'export JAVA_HOME=/usr/lib/jvm/java-8-oracle' > ~/.bashrc
+cat ~/.bashrc.bak >> ~/.bashrc
+rm ~/.bashrc.bak
