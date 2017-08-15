f9df325abe75d5d7935e65cba8aff25ed7100fd2
nathants <me@nathants.com>
2017-08-14 23:29:30 -0700

    update python36 for xenial

--- scripts/python36.sh
+++ scripts/python36.sh
@@ -3,13 +3,13 @@ set -eu
 
 sudo apt-get update
 sudo apt-get install build-essential python-virtualenv
-sudo apt-get build-dep -y python3.4
+sudo apt-get build-dep -y python3.5
 path=/opt/python36
 sudo rm -rf ${path}
 sudo mkdir -p ${path}
 sudo chown $(whoami) ${path}
 cd ${path}
-wget https://www.python.org/ftp/python/3.6.0/Python-3.6.0a4.tgz
+wget https://www.python.org/ftp/python/3.6.2/Python-3.6.2.tgz
 tar xf *
 cd *
 ./configure
