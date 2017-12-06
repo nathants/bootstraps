b56ea9f076aa0d52dc486e8d6a85537efa313797
nathants <me@nathants.com>
2017-12-05 20:24:47 -0800

    bump emacs

--- scripts/emacs.sh
+++ scripts/emacs.sh
@@ -5,8 +5,8 @@ cd ~/opt
 sudo apt-get install -y build-essential wget libncurses5-dev libgnutls-dev
 rm -rf emacs*
 # wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.xz
-wget http://ftp.gnu.org/gnu/emacs/emacs-25.2.tar.xz
-wget http://ftp.gnu.org/gnu/emacs/emacs-25.2.tar.xz.sig
+wget http://ftp.gnu.org/gnu/emacs/emacs-25.3.tar.xz
+wget http://ftp.gnu.org/gnu/emacs/emacs-25.3.tar.xz.sig
 gpg --verify emacs*.sig
 tar xf emacs*.xz
 cd emacs*
