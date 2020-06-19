a697b1b680908454f305eec60e5ff2a3d4e7aa69
nathants <me@nathants.com>
2020-06-19 12:42:25 -0700

    make bootstraps usable without git clone

--- bin/limits.sh
+++ bin/limits.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-set -eu
+set -xeuo pipefail
 
-bootstraps=$(dirname $0)
+if ! which set-opt &>/dev/null; then
+    echo '
+        fatal: no set-opt, download and chmod:
+        >> curl https://raw.githubusercontent.com/nathants/bootstraps/master/bin/set_opt.sh > /usr/local/bin/set-opt
+        >> chmod +x /usr/local/bin/set-opt
+    '
+    exit 1
+fi
 
 mkdir -p /etc/sysctl.d
-bash $bootstraps/set_opt.sh /etc/sysctl.d/99-sysctl.conf 'fs.pipe-max-size' ' = 5242880'
-bash $bootstraps/set_opt.sh /etc/sysctl.d/99-sysctl.conf 'fs.file-max' ' = 120000'
-bash $bootstraps/set_opt.sh /etc/sysctl.d/99-sysctl.conf 'fs.inotify.max_user_watches' ' = 120000'
-bash $bootstraps/set_opt.sh /etc/sysctl.d/99-sysctl.conf 'kernel.pid_max' ' = 4194303'
-bash $bootstraps/set_opt.sh /etc/sysctl.d/99-sysctl.conf 'net.core.somaxconn' ' = 8192'
+set-opt /etc/sysctl.d/99-sysctl.conf 'fs.pipe-max-size' ' = 5242880'
+set-opt /etc/sysctl.d/99-sysctl.conf 'fs.file-max' ' = 120000'
+set-opt /etc/sysctl.d/99-sysctl.conf 'fs.inotify.max_user_watches' ' = 120000'
+set-opt /etc/sysctl.d/99-sysctl.conf 'kernel.pid_max' ' = 4194303'
+set-opt /etc/sysctl.d/99-sysctl.conf 'net.core.somaxconn' ' = 8192'
 
-bash $bootstraps/set_opt.sh /etc/security/limits.conf '* - nofile' ' 120000'
-bash $bootstraps/set_opt.sh /etc/security/limits.conf '* - memlock' ' unlimited'
-bash $bootstraps/set_opt.sh /etc/security/limits.conf '* - nproc' ' 32768'
-bash $bootstraps/set_opt.sh /etc/security/limits.conf '* - as' ' unlimited'
+set-opt /etc/security/limits.conf '* - nofile' ' 120000'
+set-opt /etc/security/limits.conf '* - memlock' ' unlimited'
+set-opt /etc/security/limits.conf '* - nproc' ' 32768'
+set-opt /etc/security/limits.conf '* - as' ' unlimited'
 
-bash $bootstraps/set_opt.sh /etc/security/limits.conf 'root - nofile' ' 120000'
-bash $bootstraps/set_opt.sh /etc/security/limits.conf 'root - memlock' ' unlimited'
-bash $bootstraps/set_opt.sh /etc/security/limits.conf 'root - nproc' ' 32768'
-bash $bootstraps/set_opt.sh /etc/security/limits.conf 'root - as' ' unlimited'
+set-opt /etc/security/limits.conf 'root - nofile' ' 120000'
+set-opt /etc/security/limits.conf 'root - memlock' ' unlimited'
+set-opt /etc/security/limits.conf 'root - nproc' ' 32768'
+set-opt /etc/security/limits.conf 'root - as' ' unlimited'
