3eefd8db6c37e0ac2a8ef1a1b806a27da0e5b351
nathants <me@nathants.com>
2020-08-16 23:00:18 -0700

    simplify pytorch ami and add ubuntu variant

--- amis/pytorch_ubuntu.sh
+++ amis/pytorch_ubuntu.sh
@@ -8,20 +8,20 @@ fi
 
 id=$(aws-ec2-new bake-ami \
         --type p3.2xlarge \
-        --ami arch \
+        --ami focal \
         --spot 0 \
         --gigs 32)
 
 trap "aws-ec2-ls -s running $id && aws-ec2-rm -y $id" EXIT
 
 packages='
+    build-essential
     git
     htop
-    man
+    nvidia-cuda-toolkit
+    python3-pip
     rsync
     vim
-    nvidia-dkms
-    python-pytorch-cuda
 '
 
 pips='
@@ -46,20 +46,21 @@ aws-ec2-ssh $id -yc "
 
     echo
     echo install packages
-    sudo pacman -Syu --noconfirm --noprogress
-    sudo pacman -S --needed --noconfirm --noprogress $(echo $packages)
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    sudo apt-get install -y $(echo $packages)
 
     echo
     echo install pips
-    sudo python -m ensurepip
-    sudo python -m pip install -U pip wheel
-    sudo python -m pip install $(echo $pips)
+    sudo python3 -m pip install -U pip wheel
+    sudo python3 -m pip install $(echo $pips)
+    sudo python3 -m pip install torch==1.6.0+cu101 torchvision==0.7.0+cu101 -f https://download.pytorch.org/whl/torch_stable.html
 
 "
 
 aws-ec2-reboot $id -y
 sleep 15
 aws-ec2-wait-for-state $id -y
-ami=$(aws-ec2-ami $id -y --name pytorch)
+ami=$(aws-ec2-ami $id -y --name pytorch-ubuntu)
 aws-ec2-rm -y $id
 echo build ami: $ami
