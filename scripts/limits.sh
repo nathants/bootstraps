7d59dc1804a8eb0fd6768c5ae86b891fb8bda202
nathants <me@nathants.com>
2021-10-26 21:30:39 -1000

    mkdir on security and use alpine default limits

--- scripts/limits.sh
+++ scripts/limits.sh
@@ -8,18 +8,18 @@ fi
 
 mkdir -p /etc/sysctl.d
 set-opt /etc/sysctl.d/99-sysctl.conf 'fs.pipe-max-size' ' = 5242880'
-set-opt /etc/sysctl.d/99-sysctl.conf 'fs.file-max' ' = 120000'
-set-opt /etc/sysctl.d/99-sysctl.conf 'fs.inotify.max_user_watches' ' = 120000'
+set-opt /etc/sysctl.d/99-sysctl.conf 'fs.file-max' ' = 3240674'
+set-opt /etc/sysctl.d/99-sysctl.conf 'fs.inotify.max_user_watches' ' = 3240674'
 set-opt /etc/sysctl.d/99-sysctl.conf 'kernel.pid_max' ' = 4194303'
 set-opt /etc/sysctl.d/99-sysctl.conf 'net.core.somaxconn' ' = 8192'
 
-set-opt /etc/security/limits.conf '* - nofile' ' 120000'
+mkdir -p /etc/security
+set-opt /etc/security/limits.conf '* - nofile' ' 3240674'
 set-opt /etc/security/limits.conf '* - memlock' ' unlimited'
 set-opt /etc/security/limits.conf '* - nproc' ' 32768'
 set-opt /etc/security/limits.conf '* - as' ' unlimited'
 set-opt /etc/security/limits.conf '* hard core' ' 0'
-
-set-opt /etc/security/limits.conf 'root - nofile' ' 120000'
+set-opt /etc/security/limits.conf 'root - nofile' ' 3240674'
 set-opt /etc/security/limits.conf 'root - memlock' ' unlimited'
 set-opt /etc/security/limits.conf 'root - nproc' ' 32768'
 set-opt /etc/security/limits.conf 'root - as' ' unlimited'
