671a0bba31927909c369008d75547442e93cfe99
nathants <me@nathants.com>
2022-07-16 22:20:40 -1000

    sshd

--- scripts/limits.sh
+++ scripts/limits.sh
@@ -2,25 +2,25 @@
 set -euo pipefail
 
 if ! which set-opt &>/dev/null; then
-    curl -s https://raw.githubusercontent.com/nathants/bootstraps/master/scripts/set_opt.sh | sudo tee /usr/local/bin/set-opt >/dev/null
-    sudo chmod +x /usr/local/bin/set-opt
+    curl -s https://raw.githubusercontent.com/nathants/bootstraps/master/scripts/set_opt.sh | sudo tee /usr/bin/set-opt >/dev/null
+    sudo chmod +x /usr/bin/set-opt
 fi
 
 sudo mkdir -p /etc/sysctl.d
-/usr/local/bin/set-opt /etc/sysctl.d/99-sysctl.conf 'fs.pipe-max-size' ' = 5242880'
-/usr/local/bin/set-opt /etc/sysctl.d/99-sysctl.conf 'fs.file-max' ' = 3240674'
-/usr/local/bin/set-opt /etc/sysctl.d/99-sysctl.conf 'fs.inotify.max_user_watches' ' = 3240674'
-/usr/local/bin/set-opt /etc/sysctl.d/99-sysctl.conf 'kernel.pid_max' ' = 4194303'
-/usr/local/bin/set-opt /etc/sysctl.d/99-sysctl.conf 'net.core.somaxconn' ' = 8192'
+set-opt /etc/sysctl.d/99-sysctl.conf 'fs.pipe-max-size'            ' = 5242880'
+set-opt /etc/sysctl.d/99-sysctl.conf 'fs.file-max'                 ' = 3240674'
+set-opt /etc/sysctl.d/99-sysctl.conf 'fs.inotify.max_user_watches' ' = 3240674'
+set-opt /etc/sysctl.d/99-sysctl.conf 'kernel.pid_max'              ' = 4194303'
+set-opt /etc/sysctl.d/99-sysctl.conf 'net.core.somaxconn'          ' = 8192'
 
 sudo mkdir -p /etc/security
-/usr/local/bin/set-opt /etc/security/limits.conf '* - nofile' ' 3240674'
-/usr/local/bin/set-opt /etc/security/limits.conf '* - memlock' ' unlimited'
-/usr/local/bin/set-opt /etc/security/limits.conf '* - nproc' ' 32768'
-/usr/local/bin/set-opt /etc/security/limits.conf '* - as' ' unlimited'
-/usr/local/bin/set-opt /etc/security/limits.conf '* hard core' ' 0'
-/usr/local/bin/set-opt /etc/security/limits.conf 'root - nofile' ' 3240674'
-/usr/local/bin/set-opt /etc/security/limits.conf 'root - memlock' ' unlimited'
-/usr/local/bin/set-opt /etc/security/limits.conf 'root - nproc' ' 32768'
-/usr/local/bin/set-opt /etc/security/limits.conf 'root - as' ' unlimited'
-/usr/local/bin/set-opt /etc/security/limits.conf 'root hard core' ' 0'
+set-opt /etc/security/limits.conf '* - nofile'     ' 3240674'
+set-opt /etc/security/limits.conf '* - memlock'    ' unlimited'
+set-opt /etc/security/limits.conf '* - nproc'      ' 32768'
+set-opt /etc/security/limits.conf '* - as'         ' unlimited'
+set-opt /etc/security/limits.conf '* hard core'    ' 0'
+set-opt /etc/security/limits.conf 'root - nofile'  ' 3240674'
+set-opt /etc/security/limits.conf 'root - memlock' ' unlimited'
+set-opt /etc/security/limits.conf 'root - nproc'   ' 32768'
+set-opt /etc/security/limits.conf 'root - as'      ' unlimited'
+set-opt /etc/security/limits.conf 'root hard core' ' 0'
---
+++ scripts/sshd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+if ! which set-opt &>/dev/null; then
+    curl -s https://raw.githubusercontent.com/nathants/bootstraps/master/scripts/set_opt.sh | sudo tee /usr/bin/set-opt >/dev/null
+    sudo chmod +x /usr/bin/set-opt
+fi
+
+sudo mkdir -p /etc/ssh
+set-opt /etc/ssh/sshd_config UseDNS                   " no"
+set-opt /etc/ssh/sshd_config PasswordAuthentication   " no"
+set-opt /etc/ssh/sshd_config Compression              " no"
+set-opt /etc/ssh/sshd_config PermitRootLogin          " no"
+set-opt /etc/ssh/sshd_config Ciphers                  " chacha20-poly1305@openssh.com"
+set-opt /etc/ssh/sshd_config HostKeyAlgorithms        " ssh-ed25519"
+set-opt /etc/ssh/sshd_config KexAlgorithms            " curve25519-sha256,curve25519-sha256@libssh.org"
+set-opt /etc/ssh/sshd_config MACs                     " hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com"
+set-opt /etc/ssh/sshd_config PubkeyAcceptedAlgorithms " ssh-ed25519"
