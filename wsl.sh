19489635b1a29a70111b962b4c4fb0de601619b3
nathants <me@nathants.com>
2025-08-18 21:40:13 -1000

    uuid, gpg, ssh, win32yank

--- wsl.sh
+++ wsl.sh
@@ -39,6 +39,9 @@ sudo apt install -y \
     zip \
     expect \
     jq \
+    uuid-runtime \
+    pinentry-curses \
+    gpg \
     openssh-server
 
 bash limits.sh
@@ -49,10 +52,17 @@ chmod 700 ~/.ssh
 curl -s https://github.com/nathants.keys > ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
-sudo systemctl enable --now ssh
+sudo systemctl enable ssh
+sudo systemctl enable ssh
 
 sudo rm -f /etc/profile.d/70-systemd-shell-extra.sh
 sudo rm -f /etc/profile.d/80-systemd-osc-context.sh
+
+curl -sLo /tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/download/v0.1.1/win32yank-x64.zip
+unzip -p /tmp/win32yank.zip win32yank.exe > /tmp/win32yank.exe
+[ "dade5ae8b0bc1c029d18f260e30be1e89a3b9512bcc2904c038be75e80b02ff4" = $(sha256sum /tmp/win32yank.exe | awk '{print $1}') ]
+chmod +x /tmp/win32yank.exe
+sudo mv /tmp/win32yank.exe /usr/local/bin/
 
 ip=$(hostname -I | awk '{print $1}')
 
